9249ca4ee56ad55f71780e289f96a6345c292c7a
IgorSS <igorskor0912@gmail.com>
2024-02-17 02:37:30 +0200

    Fix bug with checking disabled fields

--- RefundCalculator/wwwroot/js/site.js
+++ RefundCalculator/wwwroot/js/site.js
@@ -1,18 +1,27 @@
-﻿function isFullCourse() {
+﻿document.addEventListener("DOMContentLoaded", function() {
+    isFullCourse();
+});
+
+function isFullCourse() {
+    var minFullCoursePrice = 991;
     var number = document.getElementById('price-course').value;
     var radio1 = document.getElementById('radio1');
     var radio2 = document.getElementById('radio2');
     var radio3 = document.getElementById('radio3');
 
-    if (number > 990) {
+    if (number >= minFullCoursePrice) {
         radio1.disabled = false;
         radio2.disabled = false;
         radio3.disabled = false;
-        radio1.checked = true;
+        if(radio3.checked === false && radio2.checked === false){
+            radio1.checked = true;
+        }
     } else {
         radio1.disabled = true;
         radio2.disabled = true;
         radio3.disabled = true;
         radio1.checked = false;
+        radio2.checked = false;
+        radio3.checked = false;
     }
 }
